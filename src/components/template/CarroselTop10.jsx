008cafb4b5618c7b14a81a6b5159fc67f15662c1
Vinícius Rovigo Medeiros <viniciusrovigomedeiros@MacBook-Air-de-Vinicius.local>
2022-05-03 13:14:51 -0300

    Clone pagina do netflix

--- src/components/template/CarroselTop10.jsx
+++ src/components/template/CarroselTop10.jsx
@@ -5,7 +5,7 @@ const CarroselTop10 = ({category}) => (
     <div className='Netflix'>
         <div className="swiper-container">
             <h2>{category.title}</h2>
-                <div className="corousel">
+                <div className="corouselTop">
                     {category.moviesTop10.map(x => (
                         <div href="swiper-slide"><img src={x.image} alt="" width={212.75} height={151.95}/></div>
                     ))}
